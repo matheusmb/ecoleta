9ae33baa6c07cdc12100c3f23d3dc5e6490e5ea3
matheusmb <matheusmb@outlook.com>
2020-06-02 21:44:17 -0300

    feat(server) create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,9 @@
 import express from "express";
-import faker from "faker";
+
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
-
-const users = [...Array(5)].map((it) => faker.name.findName());
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search).toLowerCase();
-
-  const filteredUser = users.filter((user) =>
-    user.toLowerCase().includes(search)
-  );
-
-  response.json({ users: filteredUser });
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-  response.json({ user });
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
