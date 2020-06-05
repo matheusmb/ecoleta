ad12f0fa50e71ed673b348fc03335174c9b6a450
matheusmb <matheusmb@outlook.com>
2020-06-05 00:03:32 -0300

    feat(web) integrated leaflet map

--- web/src/pages/CreatePoint/CreatePoint.tsx
+++ web/src/pages/CreatePoint/CreatePoint.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
@@ -49,6 +51,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-27.5893688, -48.5387197]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-27.5893688, -48.5387197]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
