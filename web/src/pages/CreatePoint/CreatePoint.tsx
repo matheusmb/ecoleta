fd5d3bab869005af36f30a981ee0e1102eb466f9
matheusmb <matheusmb@outlook.com>
2020-06-07 00:04:01 -0300

    feat(web) implement image upload & dropzone component

--- web/src/pages/CreatePoint/CreatePoint.tsx
+++ web/src/pages/CreatePoint/CreatePoint.tsx
@@ -7,6 +7,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 
 import api from '~/services/api';
+import DropZone from '~/components/DropZone';
 
 import logo from '~/assets/logo.svg';
 import SuccessSplash from './SuccessSplash';
@@ -35,6 +36,7 @@ const CreatePoint = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [showSuccessScreen, setShowSuccessScreen] = useState(false);
 
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -134,16 +136,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('points', data);
 
@@ -173,6 +179,8 @@ const CreatePoint = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <DropZone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
